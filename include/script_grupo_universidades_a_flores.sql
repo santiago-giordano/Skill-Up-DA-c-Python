09f291c60d7052c148d1c3333f1a5cb0466bbaff
santiago-giordano <100297383+santiago-giordano@users.noreply.github.com>
2022-11-14 20:37:07 -0300

    Changes to be committed: 	deleted:    include/P3_Uni3feb.sql 	deleted:    include/P3_UniComahue.sql 	deleted:    include/P3_UniJujuy.sql 	deleted:    include/P3_UniPalermo.sql 	deleted:    include/P3_UniSalvador.sql 	deleted:    include/P3_Uniutn.sql 	deleted:    include/script_grupo_universidades_a_flores.sql 	deleted:    include/script_grupo_universidades_a_villa_maria.sql

--- include/script_grupo_universidades_a_flores.sql
+++
@@ -1,5 +0,0 @@
-select universidad as university, carrera as career, fecha_de_inscripcion as inscription_date,
-null as first_name, name as last_name, sexo as gender, fecha_nacimiento as age, codigo_postal as postal_code, null as location,
-correo_electronico as email from flores_comahue
-where to_date(fecha_de_inscripcion, 'YYYY/MM/DD') between '2020/09/01' and '2022/02/01'
-and universidad = 'UNIVERSIDAD DE FLORES';
--- include/script_grupo_universidades_a_villa_maria.sql
+++
@@ -1,5 +0,0 @@
-select universidad as university, carrera as career, fecha_de_inscripcion as inscription_date, null as first_name,
-nombre as last_name, sexo as gender, fecha_nacimiento as age, null as postal_code, localidad as location,
-email from salvador_villa_maria svm 
-where to_date(fecha_de_inscripcion, 'DD/Mon/YY') between '01/09/20' and '01/02/22'
-and universidad = 'UNIVERSIDAD_NACIONAL_DE_VILLA_MARÍA';
